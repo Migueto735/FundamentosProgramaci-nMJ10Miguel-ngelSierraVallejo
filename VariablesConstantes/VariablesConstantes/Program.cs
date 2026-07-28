330588cd32979f3a4b5040936be7f72290320707
Migueto735 <miguela.sierra@upb.edu.co>
2026-07-28 11:31:56 -0500

    Mi primer commit

---
+++ VariablesConstantes/VariablesConstantes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //aqui escribo el codigo fuente
+            string nombre = "Miguel Ángel";
+            Console.WriteLine("Bienvenido" + nombre + "al curso de fundamentos de programación");
+        }
+    }
+}
