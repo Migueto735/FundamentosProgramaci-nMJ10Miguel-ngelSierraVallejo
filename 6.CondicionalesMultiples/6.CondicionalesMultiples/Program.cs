54126163cfaa9f27faddf2ab46175be4b14f164f
Migueto735 <miguela.sierra@upb.edu.co>
2026-08-18 11:34:03 -0500

    6.CondicionalesMultiples

---
+++ 6.CondicionalesMultiples/6.CondicionalesMultiples/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _6.CondicionalesMultiples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+              //Condicionales Múltiples
+              //Crear un algoritmo que muestre por pantalla un menú con 5 opciones, y según la opción elegida del menú, muestre un mensaje por pantalla donde se muestre la opción elegida, en caso de que se seleccione una opción que no está en el menú mostrar un mensaje de error. 
+        }
+    }
+}
